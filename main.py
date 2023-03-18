270adb36de966c03cd4d42744961f4f9f9c6291b
Defensorems <72571383+Defensorems@users.noreply.github.com>
2023-03-18 18:24:10 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,24 @@
+import telebot
+import requests
+
+# Здесь нужно заменить YOUR_API_KEY на ваш ключ API OpenWeatherMap
+API_KEY = YOUR_API_KEY
+
+#TELEGRAM_TOKEN нужно заменитть на токен вашего бота
+
+bot = telebot.TeleBot("TELEGRAM_TOKEN")
+users = {}
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    bot.reply_to(message, "Hello! I am a bot that can tell you about the weather in the selected city. When I type /remember CITY I remember your location, when I type /home I display the weather in the remembered city and /weather CITY I display the weather in the city. "
+                           "Attention! the command and the name of the city must be entered on the same line")
+
+# Обработчик команды /remember
+@bot.message_handler(commands=['remember'])
+def remember_location(message):
+    user_id = message.from_user.id
+    location = message.text.split(' ', 1)[1]
 
     # Сохраняем информацию о пользователе и его расположении в словаре
     users[user_id] = location
@@ -42,3 +63,5 @@ def get_weather(message):
         bot.reply_to(message, "Can't get data about weather")
 
 bot.polling()
+
+
